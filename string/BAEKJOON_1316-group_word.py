0595a9c2a0f50505631bbff3e5a65933cd46b32c
Hello-Hyuk <jay@inha.edu>
2022-10-19 10:36:04 +0900

    group_word

--- string/BAEKJOON_1316-group_word.py
+++ string/BAEKJOON_1316-group_word.py
@@ -5,18 +5,20 @@ n = int(sys.stdin.readline())
 words = []
 cnt = 0
 for i in range(n):
-    alpha = []
     words.append(sys.stdin.readline().strip())
+    alpha = []
     alpha.append(words[i][0])
-    for a in words[i][1:]:
-        if alpha[len(alpha)-1] != a and a not in alpha:
-            alpha.append(a)
-            if len(alpha) == len(words[i]):
-                cnt += 1
-        else : 
-            print(f"{a}")
-            break
-
+    for a in enumerate(words[i]):
+        if len(alpha) == 1:
+            alpha.append(words[i][0])       
+        else :
+            if alpha[len(alpha)-1] != a[1]:
+                if a[1] in alpha:
+                    break
+                else : alpha.append(a[1])
+        if a[0]+1 == len(words[i]):
+            cnt += 1
+    
 print(cnt)
         
             
---
+++ test.py
@@ -0,0 +1,6 @@
+list_ = ['a','b','c']
+alphat = 'a'
+
+print(alphat in list_)
+if alphat in list_:
+    print("in list")
