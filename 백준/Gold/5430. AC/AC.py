df389fbada0464efcd012e3125c942957a3a834f
최재혁 <74503770+Hello-Hyuk@users.noreply.github.com>
2023-04-12 15:13:23 +0900

    [Gold V] Title: AC, Time: 200 ms, Memory: 43636 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -1,36 +1,38 @@
+'''
+R(뒤집기): 배열에 있는 수의 순서를 뒤집는 함수
+D(버리기): 첫 번째 수를 버리는 함수 단, 빈배열때 실행 시 ERROR
+'''
 import sys
 from collections import deque
-
 input = sys.stdin.readline
-n = int(input())
-for i in range(n):
-    a = input().strip()
-    m = int(input())
+
+T = int(input())
+answer = []
+for _ in range(T):
+    p = str(input().strip())
+    n = int(input())
+    _list = deque(input().rstrip()[1:-1].split(","))
+    if n == 0:
+        _list = deque()
     flag = 1
-    arr = input().strip()
-    dq = deque(arr[1:-1].split(','))
-    if m == 0:
-        dq = deque()
-    R = 0
-    for i in range(len(a)):
-        if a[i] == 'R':
-            R += 1
-        elif a[i] == 'D':
-            if len(dq) == 0:
-                print('error')
+    for i in p:
+        if i == 'R':
+            flag *= -1
+        elif i == 'D':
+            if len(_list) != 0:
+                if flag == 1:
+                    _list.popleft()
+                elif flag == -1:
+                    _list.pop()
+            else:
+                print("error")
                 flag = 0
                 break
-            else:
-                if R % 2 == 0:
-                    dq.popleft()
-                else:
-                    dq.pop()
-
     if flag == 0:
         continue
-    else:
-        if R % 2 == 0:
-            print('[' + ",".join(dq) + ']')
-        else:
-            dq.reverse()
-            print('[' + ",".join(dq) + ']')
+    else :
+        if flag == 1:
+            print("["+",".join(_list)+"]")
+        elif flag == -1:
+            _list.reverse()
+            print("["+",".join(_list)+"]")
