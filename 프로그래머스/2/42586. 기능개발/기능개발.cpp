684dfe7e1d096c5eacad553ea55631eba15fd5d2
최재혁 <74503770+Hello-Hyuk@users.noreply.github.com>
2023-11-10 00:10:29 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,26 +1,25 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <cmath>
-
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    vector<int> days;
-    int idx=0;
-    for(int i=0;i<progresses.size();i++){
-        auto diff = ceil(float(100 - progresses[i])/float(speeds[i]));
-        days.push_back(diff);
+
+    int day;
+    int max_day = 0;
+    for (int i = 0; i < progresses.size(); ++i)
+    {
+        day = (99 - progresses[i]) / speeds[i] + 1;
+
+        if (answer.empty() || max_day < day)
+            answer.push_back(1);
+        else
+            ++answer.back();
+
+        if (max_day < day)
+            max_day = day;
     }
-    for(int i=0;i<days.size();i++){
-        if (days[idx]<days[i]){
-            answer.push_back(i-idx);
-            idx = i;
-            cout << idx << endl;
-        }
-    }   
-    answer.push_back(days.size()-idx);
-    
+
     return answer;
 }
