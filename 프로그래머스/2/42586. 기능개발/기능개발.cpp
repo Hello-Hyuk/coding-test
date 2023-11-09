d1619a1defcaaefc65b742f519c01b225fa43bfb
최재혁 <74503770+Hello-Hyuk@users.noreply.github.com>
2023-11-10 00:15:54 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,6 +1,32 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    vector<int> days;
+    int idx=0;
+    for(int i=0;i<progresses.size();i++){
+        auto diff = ceil(float(100 - progresses[i])/float(speeds[i]));
+        days.push_back(diff);
+    }
+    for(int i=0;i<days.size();i++){
+        if (days[idx]<days[i]){
+            answer.push_back(i-idx);
+            idx = i;
+        }
+    }   
+    answer.push_back(days.size()-idx);
+    
+    return answer;
+}
+/* 참고 코드 O(n)
+#include <string>
+#include <vector>
+#include <iostream>
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
@@ -23,3 +49,4 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
 
     return answer;
 }
+*/
